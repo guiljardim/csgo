7f2e1fbb22c04d0db211dee3e3752a9f578e1ac1
gjardim <guiljlima@gmail.com>
2022-07-17 18:56:36 -0300

    change service

--- app/src/main/java/com/example/csgo/data/datasource/MatchesRemoteDataSource.kt
+++ app/src/main/java/com/example/csgo/data/datasource/MatchesRemoteDataSource.kt
@@ -1,9 +1,7 @@
 package com.example.csgo.data.datasource
 
-import android.text.format.DateFormat
 import com.example.csgo.data.api.MatchesService
 import com.example.csgo.data.model.MatchRemote
-import java.util.*
 import javax.inject.Inject
 
 
@@ -12,9 +10,8 @@ class MatchesRemoteDataSource @Inject constructor(
 ) {
     suspend operator fun invoke(apiKey: String): List<MatchRemote> {
         return service.getMatchesSync(
-            "-status",
-            DateFormat.format("yyyy-MM-dd", Date()).toString(),
-            false,
+            "-status, begin_at",
+            "running, not_started",
             apiKey,
         )
     }
